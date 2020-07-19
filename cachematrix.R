e1ecc62c1f39b6112b64f2695916900ddfdc952f
Diana Barboza Primera <55921685+dianabarbo@users.noreply.github.com>
2020-07-18 22:47:11 -0500

    Commented assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 18/07/2020
+## A method for solving large matrix using lexical scoping.
+## The goal is to get the inverse of a matrix from cache, 
+## instead of calculating it again.
+## Pre-conditions: Make sure the matrix keeps unchanged.
 
-## Write a short comment describing this function
-
+# Test data
 data=matrix(1:4,2,2)
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
@@ -19,8 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
        get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
+# Computes the inverse of the object that is returned by the 
+# makeCacheMatrix fuction.
+# If the inverse has already been calculated (and the matrix has 
+# not changed), then the cachesolve should retrieve the inverse 
+# from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -35,4 +42,5 @@ cacheSolve <- function(x, ...) {
   m
 }
 
+#Testing the functions
 cacheSolve(makeCacheMatrix(data))
